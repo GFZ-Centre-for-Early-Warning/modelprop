36226e603e106935542c907210d7ca6960b3a547
Matthias Ruester <matthias.ruester@gfz-potsdam.de>
2021-11-18 15:38:03 +0100

    Apply changes from black

--- modelprop.py
+++ modelprop.py
@@ -3,12 +3,12 @@
 # Copyright © 2021 Helmholtz Centre Potsdam GFZ German Research Centre for Geosciences, Potsdam, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-# 
+#
 # https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-'''
+"""
 ModelProp
 -----------------
 Command line program to query fragility/vulnerability data from a database/file
@@ -16,7 +16,7 @@ according to a given schema and (optinally) for a set of taxonomies
 Authors:
 - Massimiliano Pittore, GFZ-Potsdam
 - Nils Brinckmann, GFZ-Potsdam
-'''
+"""
 
 import argparse
 import glob
@@ -39,12 +39,12 @@ def without_nan(list_of_dicts):
 
 
 def get_supported_schemas():
-    '''
+    """
     Searches for supported schemas in the schemas folder.
-    '''
+    """
     current_dir = os.path.dirname(__file__)
-    schema_dir = os.path.join(current_dir, 'schemas')
-    glob_str = os.path.join(schema_dir, '*', '*_struct.json')
+    schema_dir = os.path.join(current_dir, "schemas")
+    glob_str = os.path.join(schema_dir, "*", "*_struct.json")
     globbed_files = glob.glob(glob_str)
 
     supported_schemas = set()
@@ -53,18 +53,19 @@ def get_supported_schemas():
         dirname = os.path.dirname(single_file)
         parentname = os.path.basename(dirname)
 
-        if parentname + '_struct.json' == basename:
+        if parentname + "_struct.json" == basename:
             supported_schemas.add(parentname)
 
     return supported_schemas
 
 
-class Main():
-    '''
+class Main:
+    """
     Main class to execute
-    '''
+    """
+
     def __init__(self, args):
-        self.folder = os.path.dirname('__file__')
+        self.folder = os.path.dirname("__file__")
 
         self.data = None
         self.metadata = None
@@ -83,8 +84,8 @@ class Main():
         # i/o settings
         self.path_infile = self.folder
         self.in_file = self.folder
-        self.path_outfile = os.path.join(self.folder, 'output')
-        self.out_file_geojson = 'query_output.json'
+        self.path_outfile = os.path.join(self.folder, "output")
+        self.out_file_geojson = "query_output.json"
 
         # list of supported schemas
         self.supported_schemas = get_supported_schemas()
@@ -97,18 +98,18 @@ class Main():
         return set([self.schema]) <= set(self.supported_schemas)
 
     def _check_taxonomies(self, selected):
-        '''
+        """
         check if the taxonomies in the list "selected" are
         contained in the metadata
-        '''
+        """
         if self.metadata:
-            return set(selected) <= set(self.metadata['taxonomies'])
+            return set(selected) <= set(self.metadata["taxonomies"])
 
         print("_check_taxonomies: metadata are not defined.")
         return False
 
     def _read_schema(self, input_file):
-        '''
+        """
         read fragility/vulnerability model from a json file.
         the file contains two dictionaries:
         1) 'meta' includes information (metadata) on the schema,
@@ -118,16 +119,16 @@ class Main():
             distribution encoding the fragility / vulnerability
             descriptions the function returns a dictionary with
             metadata and a pandas dataframe
-        '''
-        with open(input_file, 'rt') as file_handle:
+        """
+        with open(input_file, "rt") as file_handle:
             parsed = json.load(file_handle)
 
-        self.metadata = parsed['meta']
-        self.data = pd.DataFrame(parsed['data'])
+        self.metadata = parsed["meta"]
+        self.data = pd.DataFrame(parsed["data"])
         return 0
 
     def _write_schema(self, metadata, data, output_file):
-        '''
+        """
         write fragility/vulnerability schema to a json file.
         the file contains two dictionaries:
         1) 'meta' includes information (metadata) on the schema,
@@ -136,13 +137,13 @@ class Main():
             distribution encoding the fragility / vulnerability
             descriptions the function accepts a dictionary with
             metadata and a pandas dataframe
-        '''
+        """
         if (metadata is not None) and (data is not None):
             modict = {}
-            modict['meta'] = metadata
+            modict["meta"] = metadata
             # data should be a pandas dataframe
-            modict['data'] = without_nan(data.to_dict(orient='records'))
-            with open(output_file, 'wt') as file_handle:
+            modict["data"] = without_nan(data.to_dict(orient="records"))
+            with open(output_file, "wt") as file_handle:
                 json.dump(modict, file_handle, indent=4)
             return 0
 
@@ -150,18 +151,19 @@ class Main():
         return 1
 
     def _query_model(self):
-        '''
+        """
         extract a part of the model by doing a query on the
         selected taxonomies (selectedtaxonomies)
-        '''
+        """
         if self.selectedtaxonomies:
             if self._check_taxonomies(self.selectedtaxonomies):
                 self.query_result_metadata = self.metadata.copy()
-                self.query_result_metadata['taxonomies'] = \
-                    self.selectedtaxonomies
-                self.query_result_data = self.data.set_index(
-                    'taxonomy'
-                ).loc[self.selectedtaxonomies].reset_index()
+                self.query_result_metadata["taxonomies"] = self.selectedtaxonomies
+                self.query_result_data = (
+                    self.data.set_index("taxonomy")
+                    .loc[self.selectedtaxonomies]
+                    .reset_index()
+                )
         else:
             self.query_result_data = self.data
             self.query_result_metadata = self.metadata
@@ -169,37 +171,29 @@ class Main():
         return 0
 
     def _write_outputs(self):
-        '''
+        """
         Export query result as geojson file
-        '''
+        """
         if not os.path.exists(self.path_outfile):
             os.mkdir(self.path_outfile)
-        output_geojson = os.path.join(
-            self.path_outfile,
-            self.out_file_geojson
-        )
+        output_geojson = os.path.join(self.path_outfile, self.out_file_geojson)
         self._write_schema(
-            self.query_result_metadata,
-            self.query_result_data,
-            output_geojson
+            self.query_result_metadata, self.query_result_data, output_geojson
         )
 
     def run(self):
-        '''
+        """
         Method to:
         - load the fragility model from a file (json)
         - query the model based on a list of taxonomies
         - write the output(s)
-        '''
+        """
         if self._check_schema():
-            foldername = os.path.join(
-                self.folder,
-                'schemas/{}'.format(self.schema)
-            )
+            foldername = os.path.join(self.folder, "schemas/{}".format(self.schema))
             self.path_infile = foldername
-            self.in_file = '{}_struct.json'.format(self.schema)
+            self.in_file = "{}_struct.json".format(self.schema)
         else:
-            raise Exception('schema {} not supported'.format(self.schema))
+            raise Exception("schema {} not supported".format(self.schema))
 
         # read model from file
         in_file = os.path.join(self.path_infile, self.in_file)
@@ -214,40 +208,36 @@ class Main():
 
     @classmethod
     def create_with_arg_parser(cls):
-        '''
+        """
         Creates an arg parser and uses that to create the Main class
-        '''
+        """
         arg_parser = argparse.ArgumentParser(
-            description='''Program to query a fragility/vulnerability
-            model from a database/file'''
+            description="""Program to query a fragility/vulnerability
+            model from a database/file"""
         )
         arg_parser.add_argument(
-            'schema',
-            help='Exposure/Vulnerability Schema',
+            "schema",
+            help="Exposure/Vulnerability Schema",
             type=str,
-            default='SARA_v1.0'
+            default="SARA_v1.0",
         )
         arg_parser.add_argument(
-            'assetcategory',
-            help='Type of exposed assets',
+            "assetcategory",
+            help="Type of exposed assets",
             type=str,
-            default='buildings'
+            default="buildings",
         )
         arg_parser.add_argument(
-            'losscategory',
-            help='damage or loss computation',
+            "losscategory",
+            help="damage or loss computation",
             type=str,
-            default='structural'
+            default="structural",
         )
-        arg_parser.add_argument(
-            '-taxonomies',
-            help='selected taxonomies',
-            type=str
-        )
+        arg_parser.add_argument("-taxonomies", help="selected taxonomies", type=str)
 
         args = arg_parser.parse_args()
         return cls(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Main.create_with_arg_parser().run()
--- test_cmd.py
+++ test_cmd.py
@@ -3,9 +3,9 @@
 # Copyright © 2021 Helmholtz Centre Potsdam GFZ German Research Centre for Geosciences, Potsdam, Germany
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
-# 
+#
 # https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 """
@@ -27,35 +27,20 @@ class TestTorres(unittest.TestCase):
         """
         Runs the program and tests the output.
         """
-        schema = 'Torres_Corredor_et_al_2017'
+        schema = "Torres_Corredor_et_al_2017"
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        path_modelprop = os.path.join(
-            current_dir,
-            'modelprop.py'
-        )
+        path_modelprop = os.path.join(current_dir, "modelprop.py")
 
         subprocess.run(
-            [
-                'python3',
-                path_modelprop,
-                schema,
-                'buildings',
-                'structural'
-            ],
-            check=True
-
+            ["python3", path_modelprop, schema, "buildings", "structural"], check=True
         )
-        output_file = os.path.join(
-            current_dir,
-            'output',
-            'query_output.json'
-        )
+        output_file = os.path.join(current_dir, "output", "query_output.json")
 
-        with open(output_file, 'rt') as fh:
+        with open(output_file, "rt") as fh:
             output_data = json.load(fh)
 
-        self.assertEqual(schema, output_data['meta']['id'])
+        self.assertEqual(schema, output_data["meta"]["id"])
 
 
 class TestMavrouli(unittest.TestCase):
@@ -67,35 +52,20 @@ class TestMavrouli(unittest.TestCase):
         """
         Runs the program and tests the output.
         """
-        schema = 'Mavrouli_et_al_2014'
+        schema = "Mavrouli_et_al_2014"
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        path_modelprop = os.path.join(
-            current_dir,
-            'modelprop.py'
-        )
+        path_modelprop = os.path.join(current_dir, "modelprop.py")
 
         subprocess.run(
-            [
-                'python3',
-                path_modelprop,
-                schema,
-                'buildings',
-                'structural'
-            ],
-            check=True
-
+            ["python3", path_modelprop, schema, "buildings", "structural"], check=True
         )
-        output_file = os.path.join(
-            current_dir,
-            'output',
-            'query_output.json'
-        )
+        output_file = os.path.join(current_dir, "output", "query_output.json")
 
-        with open(output_file, 'rt') as fh:
+        with open(output_file, "rt") as fh:
             output_data = json.load(fh)
 
-        self.assertEqual(schema, output_data['meta']['id'])
+        self.assertEqual(schema, output_data["meta"]["id"])
 
 
 class TestSuppasri(unittest.TestCase):
@@ -107,35 +77,20 @@ class TestSuppasri(unittest.TestCase):
         """
         Runs the program and tests the output.
         """
-        schema = 'SUPPASRI2013_v2.0'
+        schema = "SUPPASRI2013_v2.0"
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        path_modelprop = os.path.join(
-            current_dir,
-            'modelprop.py'
-        )
+        path_modelprop = os.path.join(current_dir, "modelprop.py")
 
         subprocess.run(
-            [
-                'python3',
-                path_modelprop,
-                schema,
-                'buildings',
-                'structural'
-            ],
-            check=True
-
+            ["python3", path_modelprop, schema, "buildings", "structural"], check=True
         )
-        output_file = os.path.join(
-            current_dir,
-            'output',
-            'query_output.json'
-        )
+        output_file = os.path.join(current_dir, "output", "query_output.json")
 
-        with open(output_file, 'rt') as fh:
+        with open(output_file, "rt") as fh:
             output_data = json.load(fh)
 
-        self.assertEqual(schema, output_data['meta']['id'])
+        self.assertEqual(schema, output_data["meta"]["id"])
 
 
 class TestSara(unittest.TestCase):
@@ -147,35 +102,20 @@ class TestSara(unittest.TestCase):
         """
         Runs the program and tests the output.
         """
-        schema = 'SARA_v1.0'
+        schema = "SARA_v1.0"
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        path_modelprop = os.path.join(
-            current_dir,
-            'modelprop.py'
-        )
+        path_modelprop = os.path.join(current_dir, "modelprop.py")
 
         subprocess.run(
-            [
-                'python3',
-                path_modelprop,
-                schema,
-                'buildings',
-                'structural'
-            ],
-            check=True
-
+            ["python3", path_modelprop, schema, "buildings", "structural"], check=True
         )
-        output_file = os.path.join(
-            current_dir,
-            'output',
-            'query_output.json'
-        )
+        output_file = os.path.join(current_dir, "output", "query_output.json")
 
-        with open(output_file, 'rt') as fh:
+        with open(output_file, "rt") as fh:
             output_data = json.load(fh)
 
-        self.assertEqual(schema, output_data['meta']['id'])
+        self.assertEqual(schema, output_data["meta"]["id"])
 
 
 class TestMedina(unittest.TestCase):
@@ -187,36 +127,21 @@ class TestMedina(unittest.TestCase):
         """
         Runs the program and tests the output.
         """
-        schema = 'Medina_2019'
+        schema = "Medina_2019"
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        path_modelprop = os.path.join(
-            current_dir,
-            'modelprop.py'
-        )
+        path_modelprop = os.path.join(current_dir, "modelprop.py")
 
         subprocess.run(
-            [
-                'python3',
-                path_modelprop,
-                schema,
-                'buildings',
-                'structural'
-            ],
-            check=True
-
+            ["python3", path_modelprop, schema, "buildings", "structural"], check=True
         )
-        output_file = os.path.join(
-            current_dir,
-            'output',
-            'query_output.json'
-        )
+        output_file = os.path.join(current_dir, "output", "query_output.json")
 
-        with open(output_file, 'rt') as fh:
+        with open(output_file, "rt") as fh:
             output_data = json.load(fh)
 
-        self.assertEqual(schema, output_data['meta']['id'])
+        self.assertEqual(schema, output_data["meta"]["id"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
